601f12fc13eb8eb5e5b4f9a749fff76eb17141f3
Gabriela Carvalho Camilo <gabccamilo.dev@gmail.com>
2021-04-15 19:55:22 -0300

    Adicionando a possibilidade de filtro por nome do curso ao método lista

--- src/main/java/br/com/alura/forum/repository/TopicoRepository.java
+++ src/main/java/br/com/alura/forum/repository/TopicoRepository.java
@@ -7,4 +7,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import br.com.alura.forum.modelo.Topico;
 
 public interface TopicoRepository extends JpaRepository<Topico, Long> {
+
+	List<Topico> findByCursoNome(String nomeCurso);
+
 }
